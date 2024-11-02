82824a30b6b2dcd100e7ca9c06f59f6e0c1572f0
shourok_salah <shourok_salah@codebase-tech.com>
2024-11-02 07:05:45 +0200

    design

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 //   ================== ========================
-$(document).ready(function() {
-    $('.fav-btn').click(function() {
+$(document).ready(function () {
+    $('.fav-btn').click(function () {
         $(this).toggleClass('active');
     });
-});
+    // custom sroting select
+    $(".default_option").click(function () {
+        $(this).parent().toggleClass("active");
+        $(".sorting-block").toggleClass("active")
+    })
+
+    $(".select_ul li").click(function () {
+        var currentele = $(this).html();
+        $(".default_option li").html(currentele);
+        $(this).parents(".select_wrap").removeClass("active");
+        $(".sorting-block").removeClass("active")
+    })
  
+});
